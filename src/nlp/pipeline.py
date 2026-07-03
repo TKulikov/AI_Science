d95891a41309ff2a9d3e938f625e68f6f6d8900a
Morgot <morgot140@gmail.com>
2026-07-03 15:11:53 +0300

    test api and create api security

---
+++ src/nlp/pipeline.py
@@ -0,0 +1,18 @@
+from yandex_ai_studio_sdk import AIStudio
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+folder_id = os.getenv("FOLDER_ID")
+api_key = os.getenv("API_KEY")
+if api_key is None or folder_id is None:
+    print("get env key failed")
+    raise RuntimeError
+sdk = AIStudio(folder_id=folder_id, auth=api_key)
+
+model = sdk.models.completions("yandexgpt")
+model = model.configure(temperature=0.5)
+result = model.run("foo")
+
+for alternative in result:
+    print(alternative)
